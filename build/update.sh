0b003c4d5ac30404981230662ce02d7e975f8e62
hz9527 <981379316@qq.com>
2017-11-18 17:50:01 +0800

    test sh

--- build/update.sh
+++ build/update.sh
@@ -1,11 +1,11 @@
-read -p "are you sure publish (y/n) " -n 1 -r
+read -p "are you sure publish, do not remember push your code (y/n) " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   npm run build
+  git checkout gh-pages
+  node rebuild.js
+  git status
+  git add .
+  git commit -m "update pages"
+  git push origin gh-pages
 fi
-git checkout gh-pages
-node rebuild.js
-git status
-git add .
-git commit -m "update pages"
-git push origin gh-pages
